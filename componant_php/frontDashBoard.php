fe05343a2a05251b0b88a61267cebedf34ec7a93
Elise MASCRET <elise.mascret@viacesi.fr>
2023-09-12 14:34:36 +0200

    test64

---
+++ componant_php/frontDashBoard.php
@@ -0,0 +1,36 @@
+<?php
+ echo '<div class="top_dashboard">
+ <div class="compte"><p>Information du compte</p>
+        <div class="infos">
+            <p>Nom du compte</p>
+            <p>Numéro de téléphone</p>
+            <p>E-mail :</p>
+            <p>Type, admin ou employés</p>
+            <p>Adresse ?</p>
+            <p>Pays</p>
+        </div>
+    </div>
+    <div class="agenda"><p>Agenda</p>
+        <div class="rdv">
+            <div><p>Demain</p></div>
+            <div><p>Après-demain</p></div>
+            <div><p>3ème jour</p></div>
+        </div>
+    </div>
+    <div class="commentaire"><p>Derniers commentaires des clients</p>
+        <div class="derniers"><p>Lorem ipsum dolor sit amet, <br>consectetur adipiscing elit,
+            <br>sed  eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
+    </div>
+    </div>
+    <div class="bottom_dashboard">
+    <div class="mail"><p>Derniers mails</p>
+        <div class="boite"><p>Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit,
+            <br> sed  eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
+    </div>
+    <div class="vente"><p>Dernières ventes</p>
+        <div class="graphique"><p>Lorem ipsum dolor sit amet, <br>consectetur adipiscing elit,
+            <br>sed  eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
+    </div>
+    </div>';
+
+?>
