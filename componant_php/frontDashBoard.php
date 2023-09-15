1a37c4bb2c67217fe631c2c6331f22c1713a4410
Elise MASCRET <elise.mascret@viacesi.fr>
2023-09-15 16:20:48 +0200

    maj css 15/09/23

--- componant_php/frontDashBoard.php
+++ componant_php/frontDashBoard.php
@@ -1,6 +1,6 @@
 <?php
  echo '<div class="top_dashboard">
- <div class="compte"><p class="infos_compte">Information du compte</p>
+ <div class="compte"><p class="infos_compte"><b>Information du compte</b></p>
         <div class="description">
             <p>Nom du compte</p>
             <p>Numéro de téléphone</p>
@@ -10,24 +10,24 @@
             <p>Pays</p>
         </div>
     </div>
-    <div class="agenda"><p class="planning">Agenda</p>
+    <div class="agenda"><p class="planning"><b>Agenda</b></p>
         <div class="rdv">
             <div class="demain"><p>Demain</p></div>
             <div class="apres-demain"><p>Après-demain</p></div>
             <div class="jour3"><p>3ème jour</p></div>
         </div>
     </div>
-    <div class="commentaire"><p class="clients">Derniers commentaires des clients</p>
+    <div class="commentaire"><p class="clients"><b>Derniers commentaires des clients</b></p>
         <div class="derniers"><p>Lorem ipsum dolor sit amet, <br>consectetur adipiscing elit,
             <br>sed  eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
     </div>
     </div>
     <div class="bottom_dashboard">
-    <div class="mail"><p>Derniers mails</p>
+    <div class="mail"><p class="derniersMails"><b>Derniers mails</b></p>
         <div class="boite"><p>Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit,
             <br> sed  eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
     </div>
-    <div class="vente"><p>Dernières ventes</p>
+    <div class="vente"><p class="dernieresVentes"><b>Dernières ventes</b></p>
         <div class="graphique"><p>Lorem ipsum dolor sit amet, <br>consectetur adipiscing elit,
             <br>sed  eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
     </div>
