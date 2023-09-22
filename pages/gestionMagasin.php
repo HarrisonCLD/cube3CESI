cd0deff62578d5dc23eadf279a97b20c14148ae7
Pousset <62299589+Pousset@users.noreply.github.com>
2023-09-22 16:33:21 +0200

    Ajout page gestion + ressources humaine 22/09 v2

--- pages/gestionMagasin.php
+++ pages/gestionMagasin.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion Magasin</title>
-    <link  rel="stylesheet" href="../css/navBarV2.css">
+    <link  rel="stylesheet" href="../css/index.css">
 </head>
 <body>
     
